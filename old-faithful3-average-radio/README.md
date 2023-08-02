57cdd6fee8759e5b66fb9052df0d3489bd02f5dc
Gaston Sanchez <gaston.stat@gmail.com>
2023-08-02 08:19:27 -0600

    old-faithful: add version 3

---
+++ old-faithful3-average-radio/README.md
@@ -0,0 +1,41 @@
+# Old Faithful
+
+This is a Shiny app that graphs a histogram of the variables in the `faithful`
+data frame, which is based on the famous _Old Faithful_ geyser located in 
+Yellowstone National Park.
+
+<https://en.wikipedia.org/wiki/Old_Faithful>
+
+This Shiny app is a modified version of the default _Old Faithful_ app. In 
+addition to the input slider for selecting the number of bins, this
+app also includes: 
+
+- a select input-widget to choose the variable to be graphed.
+
+- radio-buttons to decide whether to display a line for the 
+average value (of the chosen variable)
+
+
+## Data
+
+This app uses the built-in data set `faithful` which is a data frame with
+272 observations on 2 variables.
+
+- `eruptions`: Eruption time in minutes.
+
+- `waiting`: Waiting time to next eruption (in minutes).
+
+
+## How to run it?
+
+One quick way to run the app is with the `"shiny"` function `runGitHub()` as follows:
+
+```R
+library(shiny)
+
+runGitHub(
+  repo = "shiny", 
+  username = "data133", 
+  subdir = "old-faithful3-average-radio")
+```
+
---
+++ old-faithful3-average-radio/app.R
@@ -0,0 +1,78 @@
+# Title: Old Faithful geyser histogram
+# Description: Histogram of waiting time 
+# Details: uses radio buttons to decide whether to show average line
+# Author: Gaston Sanchez
+# Date: Summer 2023
+
+
+# ===============================================
+# Packages
+# ===============================================
+library(shiny)
+
+
+# ===========================================================
+# Define UI for application that graphs a histogram
+# ===========================================================
+ui <- fluidPage(
+  
+  # Application title
+  titlePanel("Old Faithful Geyser Data"),
+  
+  # -------------------------------------------------------
+  # Sidebar with input widgets 
+  # -------------------------------------------------------
+  sidebarLayout(
+    sidebarPanel(
+      selectInput(inputId = "variable",
+                  label = "Variable",
+                  choices = names(faithful),
+                  selected = "waiting"),
+      sliderInput("bins",
+                  "Number of bins:",
+                  min = 1,
+                  max = 50,
+                  value = 30),
+      radioButtons(inputId = "show_avg", 
+                   label = "Show average", 
+                   choices = c("no", "yes"),
+                   selected = "no")
+    ),
+    
+    # ----------------------------------------------------------
+    # Main Panel with output: plot of the generated distribution
+    # ----------------------------------------------------------
+    mainPanel(
+      plotOutput("distPlot")
+    )
+  ) # closes sidebarLayout
+) # closes fluidPage
+
+
+# ======================================================
+# Define server logic required to graph the histogram
+# ======================================================
+server <- function(input, output) {
+  
+  output$distPlot <- renderPlot({
+    # generate bins based on input$bins from ui.R
+    x    <- faithful[, input$variable]
+    bins <- seq(min(x), max(x), length.out = input$bins + 1)
+    
+    # draw the histogram with the specified number of bins
+    hist(x, breaks = bins, col = 'darkgray', border = 'white',
+         xlab = 'Waiting time to next eruption (in mins)',
+         main = paste0('Histogram of ', input$variable, ' times'))
+    
+    # decide whether to show line of average waiting time
+    if (input$show_avg == "yes") {
+      avg_time = mean(x)
+      abline(v = avg_time, col = "red", lwd = 3)
+    } else {
+      NULL
+    }
+  })
+}
+
+# Run the application 
+shinyApp(ui = ui, server = server)
