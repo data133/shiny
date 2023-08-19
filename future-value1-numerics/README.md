b5043a2f40ee174d7a6e2aacae0b132c766d4a2e
Gaston Sanchez <gaston.stat@gmail.com>
2023-08-19 07:54:17 -0600

    future value 1

---
+++ future-value1-numerics/README.md
@@ -0,0 +1,31 @@
+# Simple Future Value Timeline
+
+This is a Shiny app that produces a timeline to visualize a simple Future Value
+calculation of a lump sum investment.
+
+
+## Inputs
+
+This app takes three inputs (using numeric input widgets):
+
+- Initial amount
+
+- Interest Rate (i.e. annual rate of return)
+
+- Years
+
+
+
+## How to run it?
+
+One quick way to run the app is with the `"shiny"` function `runGitHub()` as follows:
+
+```R
+library(shiny)
+
+runGitHub(
+  repo = "shiny", 
+  username = "data133", 
+  subdir = "future-value1-numerics")
+```
+
---
+++ future-value1-numerics/app.R
@@ -0,0 +1,79 @@
+# Title: Future Value (compound interest)
+# Description: Shiny app that graphs a FV timeline
+# Details: uses three numeric input widgets
+# Author: Gaston Sanchez
+# Date: Spring 2023
+# Refs: 
+# - https://www.investopedia.com/terms/f/futurevalue.asp
+
+
+# ===============================================
+# Packages
+# ===============================================
+library(shiny)
+
+
+# ===========================================================
+# Define UI for application that graphs a FV timeline
+# ===========================================================
+ui <- fluidPage(
+  
+  # Application title
+  titlePanel("Future Value"),
+  
+  # -------------------------------------------------------
+  # Sidebar with input widgets 
+  # -------------------------------------------------------
+  sidebarLayout(
+    sidebarPanel(
+      numericInput(inputId = "amount",
+                  label = "Initial Amount",
+                  min = 1,
+                  value = 1000),
+      numericInput(inputId = "rate",
+                  label = "Interest Rate (%)",
+                  min = 0,
+                  value = 5),
+      numericInput(inputId = "years",
+                  label = "Number of Years",
+                  min = 1,
+                  value = 10)
+    ),
+    
+    # -------------------------------------------------------
+    # Main Panel with output: plot of the generated distribution
+    # -------------------------------------------------------
+    mainPanel(
+      plotOutput(outputId = "graphic")
+    )
+  ) # closes sidebarLayout
+) # closes fluidPage
+
+
+# ======================================================
+# Define server logic required to graph the timeline
+# ======================================================
+server <- function(input, output) {
+  
+  output$graphic <- renderPlot({
+    time = 0:input$years
+    fv = input$amount * (1 + input$rate/100)^time
+    # main timeline
+    plot(x = time, 
+         y = fv, 
+         type = "l",            # line (type of plot)
+         lwd = 2,               # line width
+         las = 1,               # style of axis labels
+         col = "blue",          # color
+         xlab = "time (years)", # x-axis label
+         ylab = "amount",       # y-axis label
+         main = "Future Value"  # title
+    )
+    # add points
+    points(x = time, y = fv, col = "blue", pch = 20)
+  })
+}
+
+
+# Run the application 
+shinyApp(ui = ui, server = server)
