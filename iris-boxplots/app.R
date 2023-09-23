876bac740a7f76ef4ea378579a61fe78cd9d1ac2
Gaston Sanchez <gaston.stat@gmail.com>
2023-09-23 09:19:06 -0700

    iris-boxplots

---
+++ iris-boxplots/app.R
@@ -0,0 +1,86 @@
+# title: Univariate exploration of Iris data.
+# description: Graphs a histogram, and computes descriptive statistics
+# for a selected variable from the iris data.
+# author: Gaston Sanchez
+# date: Summer 2023
+
+
+# ===============================================
+# Packages
+# ===============================================
+library(shiny)
+library(tidyverse)
+
+
+# =======================================================
+# Define UI for graphing histograms, and computing summary stats
+# =======================================================
+ui <- fluidPage(
+  
+  # Application title
+  titlePanel("Iris Data"),
+  
+  # -------------------------------------------------------
+  # Sidebar with input widgets 
+  # -------------------------------------------------------
+  sidebarLayout(
+    sidebarPanel(
+      selectInput(inputId = "variable", 
+                  label = "Select a variable", 
+                  choices = names(iris)[-5], 
+                  selected = "Sepal.Length"),
+      sliderInput(inputId = "bins",
+                  label = "Number of bins",
+                  min = 1, 
+                  max = 30,
+                  value = 10)
+    ), # closes sidebarPanel of inputs
+    
+    # -------------------------------------------------------
+    # Main Panel with output: histograms, and table of summary statistics
+    # -------------------------------------------------------
+    mainPanel(
+      h3("Histograms"),
+      plotOutput(outputId = "graphic"),
+      hr(),
+      h3("Summary Stats"),
+      tableOutput(outputId = "summary")
+    )
+    
+  ) # closes sidebarLayout
+) # closes fluidPage
+
+
+# ==============================================================
+# Server logic to draw a histograms, and compute summary stats
+# ==============================================================
+server <- function(input, output) {
+  
+  # output histograms
+  output$graphic <- renderPlot({
+    # notice the use of "tidy evaluation" when mapping the variable!!!
+    ggplot(iris, aes(x = .data[[input$variable]])) +
+      geom_histogram(bins = input$bins, color = "white", aes(fill = Species)) +
+      facet_wrap(~ Species) +
+      theme(legend.position = "none")
+  })
+  
+  
+  # output table
+  output$summary <- renderTable({
+    # notice the use of "tidy evaluation" when referring to the variable!!!
+    iris %>%
+      group_by(Species) %>%
+      summarise(
+        min = min(.data[[input$variable]]),
+        mean = mean(.data[[input$variable]]),
+        sd = sd(.data[[input$variable]]),
+        max = max(.data[[input$variable]])
+      )
+  })
+  
+} # closes server
+
+
+# Run the application 
+shinyApp(ui = ui, server = server)
